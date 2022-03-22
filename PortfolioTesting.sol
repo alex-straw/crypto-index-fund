81ed0663a982cec0d63b9fb1212e7adeefc28cdf
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-22 17:53:01 +0000

    Update PortfolioTesting.sol
    Basic functionality required to work with frontend

--- PortfolioTesting.sol
+++ PortfolioTesting.sol
@@ -3,5 +3,33 @@
 pragma solidity ^0.8.7;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Vault.sol";
 
-contract PortfolioTesting {}
+contract PortfolioTesting is ERC20 {
+
+    Vault vault;
+    uint256 ethToTokenRatio; 
+    address[] tokenAddresses;
+    uint256[] percentageHoldings;
+
+    constructor() {
+        ethToTokenRatio = 1000;
+        tokenAddresses = ["0xc778417E063141139Fce010982780140Aa0cD5Ab"];
+        percentageHoldings = [100];
+        vault = new Vault(tokenAddresses);
+    }
+
+    function buy() public payable returns(uint256) {
+        uint256 memory tokensToMint = msg.value * ethToTokenRatio; 
+        vault.deposit("0xc778417E063141139Fce010982780140Aa0cD5Ab", msg.sender, msg.value);
+        _mint(msg.sender, tokensToMint);
+        return (tokensToMint);
+    }
+
+    function sell(uint256 tokensToSell) public returns(uint256) {
+        uint256 memory ethToWithdraw = tokensToSell/ethToTokenRatio;
+        vault.withdraw("0xc778417E063141139Fce010982780140Aa0cD5Ab", ethToWithdraw);
+        _burn(msg.sender, tokensToSell);
+        return(ethToWithdraw);
+    }
+}
