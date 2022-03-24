d8291450533bfbef3297f24eda8245b2de621eda
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-03-24 17:34:28 +0000

    made constructor payable and implemented swap for Weth

--- TestPortfolio.sol
+++ TestPortfolio.sol
@@ -10,14 +10,18 @@ contract TestPortfolio is ERC20 {
     uint256 public ethToTokenRatio;
     address[] public tokenAddresses;
     uint256[] public percentageHoldings;
+    address payable WETH = payable(0xc778417E063141139Fce010982780140Aa0cD5Ab);
 
     constructor(string memory name_, string memory symbol_)
+        payable
         ERC20(name_, symbol_)
     {
         ethToTokenRatio = 1000;
         tokenAddresses.push(0xc778417E063141139Fce010982780140Aa0cD5Ab);
         percentageHoldings.push(100);
         vault = new Vault(tokenAddresses);
+        (bool sent, bytes memory data) = WETH.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
 
     function buy() public payable {
@@ -38,5 +42,15 @@ contract TestPortfolio is ERC20 {
             ethToWithdraw
         );
         _burn(msg.sender, tokensToSell);
+    }
+
+    // FUNCTIONS FOR DEBUGGING
+
+    function getEthBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getWethBalance() public view returns (uint256) {
+        return ERC20(WETH).balanceOf(address(this));
     }
 }
