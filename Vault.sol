8b5728fc96ca91a8578515a921b654b9beac59d6
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-03-25 10:46:41 +0000

    changed vault to also track total ownership

--- Vault.sol
+++ Vault.sol
@@ -6,16 +6,19 @@ pragma solidity ^0.8.7;
 // Needs to track who owns each of those coins
 
 contract Vault {
-    // assets: Token address -> user address -> quantity owned
-    mapping(address => mapping(address => uint256)) private assets;
-
+    // totalAssets: token address => total quantity owned
+    mapping(address => uint256) public totalAssets;
+    // Tracks user ownership of assets in vault
+    // userAssets: token address -> user address -> quantity owned by user
+    mapping(address => mapping(address => uint256)) private userAssets;
     address portfolioAddress;
 
     constructor(address[] memory tokenAddresses) {
         portfolioAddress = msg.sender;
 
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
-            assets[tokenAddresses[i]][portfolioAddress] = 0;
+            totalAssets[tokenAddresses[i]] = 0;
+            userAssets[tokenAddresses[i]][portfolioAddress] = 0;
         }
     }
 
@@ -29,7 +32,8 @@ contract Vault {
         address userAddress,
         uint256 amount
     ) external isPortfolio {
-        assets[tokenAddress][userAddress] += amount;
+        totalAssets[tokenAddress] += amount;
+        userAssets[tokenAddress][userAddress] += amount;
     }
 
     function withdraw(
@@ -38,9 +42,10 @@ contract Vault {
         uint256 amount
     ) external isPortfolio {
         require(
-            assets[tokenAddress][userAddress] >= amount,
+            userAssets[tokenAddress][userAddress] >= amount,
             "Insufficient funds"
         );
-        assets[tokenAddress][userAddress] -= amount;
+        totalAssets[tokenAddress] -= amount;
+        userAssets[tokenAddress][userAddress] -= amount;
     }
 }
