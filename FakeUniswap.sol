d3a1a32cee3b0b0ab4862edbcb2e6d6c98b88c25
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-27 18:08:00 +0000

    Update FakeUniswap.sol
    Added Rinkeby address for deployed and loaded (pre-paid) contract.

--- FakeUniswap.sol
+++ FakeUniswap.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+// Rinkeby Address: 0x1fCb5Ef0826112f904ABdBBB0493158737Ceb726
+
 contract FakeUniswap {
 
     // Fake Uniswap contract for testing multi-ERC20 portfolios
