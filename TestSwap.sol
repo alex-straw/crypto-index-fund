7087e10595501c4a2bee42409dafe1b8480f3b96
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-28 14:59:11 +0000

    Update TestSwap.sol
    Made addresses constant.  Removed excess functions to minimise deployment gas.  Created 'contractBalanceOf' function to retrieve the contract's balance of specific tokens for debugging

--- TestSwap.sol
+++ TestSwap.sol
@@ -14,26 +14,17 @@ interface IfakeUniswap {
 
 contract TestSwap {
 
-    address fakeUniswap = 0x1fCb5Ef0826112f904ABdBBB0493158737Ceb726;
-
-    address DAI = 0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa;
-    address LINK = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
-    address WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+    address constant fakeUniswap = 0x1fCb5Ef0826112f904ABdBBB0493158737Ceb726;
+    address constant DAI = 0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa;
+    address constant LINK = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    address constant WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
 
     function swapToken(uint256 _amountWethToSell, address _recipient, address _tokenToBuy) public {
         IERC20(WETH).transfer(fakeUniswap, _amountWethToSell);
         IfakeUniswap(fakeUniswap).swapWethForToken(_tokenToBuy, _recipient, _amountWethToSell);
     }
 
-    function contractDAIBalance() public view returns(uint256) {
-        return IERC20(DAI).balanceOf(address(this));
-    }
-
-    function contractLINKBalance() public view returns(uint256) {
-        return IERC20(LINK).balanceOf(address(this));
-    }
-
-    function contractWETHBalance() public view returns(uint256) {
-        return IERC20(WETH).balanceOf(address(this));
+    function contractBalanceOf(_tokenAddress) public view returns(uint256) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
     }
 }
