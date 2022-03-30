d72414f8ca9383a05bdd637ec4aa4bab8aab4611
alex-straw <as17163@bristol.ac.uk>
2022-03-30 11:43:59 +0000

    added returns function in the FakeUniswap interface

--- contracts_dev/TestSwap.sol
+++ contracts_dev/TestSwap.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 // Needs to track who owns each of those coins
 
 interface IfakeUniswap {
-    function swapWethForToken(address _tokenToBuy, address _recipient, uint256 _amountWethToSell) external;
+    function swapWethForToken(address _tokenToBuy, address _recipient, uint256 _amountWethToSell) external returns(uint256);
     function increment() external;
 }
 
