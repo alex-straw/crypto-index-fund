3bd90fb5de7972070fba106d7fa65a9a1085dd92
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-30 23:25:47 +0000

    Update MVPPortfolio.sol
    Removed unnecessary modifiers and now calling initialise portfolio within the constructor.  Made the initialisePortfolio function private too.

--- contracts_dev/MVPPortfolio.sol
+++ contracts_dev/MVPPortfolio.sol
@@ -31,7 +31,6 @@ contract MVPPortfolio is ERC20 {
     uint256 public _percentageWethAmount;
 
     address public Owner;
-    bool initialised = false;
 
     constructor(
         string memory name_,
@@ -53,11 +52,12 @@ contract MVPPortfolio is ERC20 {
         vault = new Vault(tokenAddresses_);
         ethToWeth();
         Owner = msg.sender;
+        initialisePortfolio();
     }
     
     // ------------------------------  Initalise Portfolio ----------------------------------- //
 
-    function initialisePortfolio() onlyOwner notInitialised public {
+    function initialisePortfolio() private {
         _totalWethAmount = getBalance(0xc778417E063141139Fce010982780140Aa0cD5Ab, address(this));
         for (uint i=0; i < tokenAddresses.length; i++) {
             _percentageWethAmount = _totalWethAmount * percentageHoldings[i] / 100;
@@ -96,7 +96,7 @@ contract MVPPortfolio is ERC20 {
     
     // ---------------------------------- Buy / Sell / Deposit ---------------------------------- //
 
-    function buy() public payable isInitialised {
+    function buy() public payable {
         ethToWeth();
         uint256 vaultValuePrior = deposit(msg.value);
         // The number of tokens to mint is determined by the formula:
@@ -110,7 +110,7 @@ contract MVPPortfolio is ERC20 {
         _mint(msg.sender, tokensToMint);
     }
 
-    function sell(uint256 tokensToSell) public isInitialised {
+    function sell(uint256 tokensToSell) public {
         for (uint i = 0; i < tokenAddresses.length; i++) {
             // How much of the token do they own?
             uint256 userAssets = vault.getUserQuantity(
@@ -127,7 +127,7 @@ contract MVPPortfolio is ERC20 {
         _burn(msg.sender, tokensToSell);
     }
 
-    function deposit(uint256 _totalWethAmount) private isInitialised returns (uint256) {
+    function deposit(uint256 _totalWethAmount) private returns (uint256) {
         uint256 vaultValuePrior = 0;
         for (uint i = 0; i < tokenAddresses.length; i++) {
             // Swap WETH for a different token which is transferred to the vault
@@ -159,16 +159,6 @@ contract MVPPortfolio is ERC20 {
  
     modifier onlyOwner() {
         require(Owner==msg.sender);
-        _;
-    }
-
-    modifier isInitialised() {
-        require(initialised==true);
-        _;
-    }
-
-    modifier notInitialised() {
-        require(initialised==false);
         _;
     }
 }
