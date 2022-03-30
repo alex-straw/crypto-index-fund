cf8cbcffd62a9fbb7cc16e202ed58d89d5ad92b0
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-30 22:54:12 +0000

    Update MVPPortfolio.sol
    Changed to just have IERC20 imported - more lightweight

--- contracts_dev/MVPPortfolio.sol
+++ contracts_dev/MVPPortfolio.sol
@@ -2,8 +2,7 @@
 
 pragma solidity ^0.8.7;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Vault.sol";
 
 // Example portfolio of Weth and Dai
