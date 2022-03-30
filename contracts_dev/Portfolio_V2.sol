4a39a1c606d8620d8b88253d4212a680b75e1f48
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2022-03-30 17:14:44 +0000

    Update Portfolio_V2.sol
    TODO

--- contracts_dev/Portfolio_V2.sol
+++ contracts_dev/Portfolio_V2.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Vault.sol";
 
+// TODO:
+// Burn 1% of initial FOLO coins so that the contract doesn't die when all token holders sell 
+
 
 // ------------------------------ Temporary Interface for Fake Uniswap ------------------------------ // 
 
