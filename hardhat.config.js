248249dd588d38728b0ff2dec5c8506822c7cc2e
alex-straw <as17163@bristol.ac.uk>
2022-04-08 16:06:29 +0000

    changed name from Infura ID to Rinkeby ID

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,9 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 console.log(process.env);
-const INFURA_API_KEY = process.env.INFURA_API;
-const KOVAN_API_KEY = process.env.KOVAN_API;
-const PRIVATE_KEY = process.env.WALLET_PK;
+const RINKEBY_API_KEY = process.env.RINKEBY_API_KEY;
+const KOVAN_API_KEY = process.env.KOVAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -13,7 +13,7 @@ module.exports = {
   solidity: "0.8.7",
   networks: {
     rinkeby: {
-      url: `${INFURA_API_KEY}`,
+      url: `${RINKEBY_API_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
     },
     kovan: {
