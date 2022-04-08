9297ffbe5f144f3d25d0a360cb96218f399008de
alex-straw <as17163@bristol.ac.uk>
2022-04-08 19:24:40 +0000

    added test file for deploying portfolio

---
+++ test.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+
+describe("Portfolio Launch", function () {
+  it("Portfolio should be created", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
