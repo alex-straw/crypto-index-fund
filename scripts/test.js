16c11657786f35fb8860604816ba17c8490e01c1
alex-straw <as17163@bristol.ac.uk>
2022-04-09 20:25:56 +0100

    now correctly instantiating portfolio as a contract whose functions can be called

--- scripts/test.js
+++ scripts/test.js
@@ -4,26 +4,33 @@ var it = mocha.it
 var expect = require('chai').expect
 var assert = require('chai').assert
 var chai = require('chai');  
-
+const { ethers } = require('hardhat')
 
 async function getTotalSupply(contract) {
   return (await contract.totalSupply.call()).toString()
 }
 
 async function main() {
+  // --------------------------- GENERAL --------------------------- //
+
+  // Generic state variables for assertions/expect statements:
+
+  let INITIALISE_AMOUNT = "10000000000"
+  let CHAINLINK_ADDRESS = "0xa36085F69e2889c224210F603D836748e7dC0088"
+  let WETH_ADDRESS = "0xd0A1E359811322d97991E03f863a0C30C2cF029C"
+
 
   // --------------------------- Constructor --------------------------- //
+
   let _name = "Portfolio";
   let _ticker = "FOLO";
   let _tokenAddresses = ["0xa36085F69e2889c224210F603D836748e7dC0088", "0xd0A1E359811322d97991E03f863a0C30C2cF029C"];
   let _percentageHoldings = [40, 60];
   let _ownerFee = 100;
-  let _initialiseAmount = "10000000000"
 
   // ---------------------------- Initialise ---------------------------- //
 
   /*
-
   PROCESS JUSTIFICATION:
   
   Typically Hardhat is used to deploy contracts on a local blockchain for quick testing of 
@@ -41,9 +48,8 @@ async function main() {
   Consequently, to get these tests running it requires real test Ether and a different strategy
   for testing than is recommended in Hardhat tutorials.  Also, I've upped the timeout time for
   MOCHA to around 40,000 ms as these transactions must be mined into a real block.  
-
-
   */
+
 
   /*
   1. First deploy PortfolioFactory.sol
@@ -54,25 +60,29 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Portfolio = await ethers.getContractFactory("PortfolioFactory");
-  const PortfolioFactory = await Portfolio.deploy();
+  const PortfolioFactory = await ethers.getContractFactory("PortfolioFactory");
+  const portfolioFactory = await PortfolioFactory.deploy();
 
-  await PortfolioFactory.create(
+  await portfolioFactory.create(
     _name,
     _ticker,
     _tokenAddresses,
     _percentageHoldings,
     _ownerFee);
 
-  const PortfolioAddress = await PortfolioFactory.portfolios(0);  // Get the portfolio address
-  await PortfolioAddress.initialisePortfolio({value: _initialiseAmount});  // Initialise the portfolio
+  const _portfolioAddress = await portfolioFactory.portfolios(0);  // Get the portfolio address
+
+  const Portfolio = await ethers.getContractFactory("Portfolio");
+  const portfolio = await Portfolio.attach(_portfolioAddress);
+
+  await portfolio.initialisePortfolio({value: INITIALISE_AMOUNT});  // Initialise the portfolio
   
-  console.log("Portfolio factory address:", await PortfolioFactory.address)
-  console.log("Portfolio address: ", await PortfolioAddress)
+  console.log("Portfolio factory address:", await portfolioFactory.address)
+  console.log("Portfolio address: ", await portfolio.address)
 
   // ---------------------------- Init Testing ---------------------------- //
-  assert.equal(await getTotalSupply(hardhatPortfolio), "100000000000000000000");
 
+  //assert.equal(await getTotalSupply(portfolio), "100000000000000000000");
 }
 
 main()
