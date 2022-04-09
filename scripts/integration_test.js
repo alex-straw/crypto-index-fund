07f5a4a76a5d275149a4f30f9dedf3ed94baf822
alex-straw <as17163@bristol.ac.uk>
2022-04-09 23:12:16 +0100

    added a second mocha test that checks the token supply after initialisation

--- scripts/integration_test.js
+++ scripts/integration_test.js
@@ -9,6 +9,9 @@ let _percentageHoldings = [40, 60];
 let _owner = "0xF1C37BC188643DF4Bf15Fd437096Eb654d30abc1"
 let _ownerFee = 100;
 
+// Check variables
+const OWNER = "0xF1C37BC188643DF4Bf15Fd437096Eb654d30abc1"
+const INITIALISE_AMOUNT = "10000000000" 
 
 describe('MyFirstContract Integration Test', function () {
     before(async function () {
@@ -23,11 +26,25 @@ describe('MyFirstContract Integration Test', function () {
             _ownerFee
         );
         await portfolio.deployed();
+        //await portfolio.initialisePortfolio({value:INITIALISE_AMOUNT});
     });
 
     it('has a total supply of 0', async function () {
         let result = await portfolio.totalSupply.call();
         console.log('total supply: ', result)
         expect(await result.toString()).to.equal("0");
+    });
+
+    describe('Initialisation testing', function () {
+        before(async function () {
+            await portfolio.initialisePortfolio({value:INITIALISE_AMOUNT});
+        });
+
+
+        it('has a total supply of 1000000000000000', async function () {
+            let result = await portfolio.totalSupply.call();
+            console.log('total supply: ', result)
+            expect(await result.toString()).to.equal("100000000000000000000");
+        });
     });
 });
